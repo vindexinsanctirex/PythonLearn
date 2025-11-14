6dcad14e3e045e574100d90e18f05c5db0ba2d79
Caio S. Duarte <caiosduarte@protonmail.com>
2025-11-14 20:41:19 +0000

    att

---
+++ Aula 1 - Lógica de Programação/entrada_de_dados.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+print(f"Seu nome é {nome}, você tem {idade} anos e {altura}m de altura.")
