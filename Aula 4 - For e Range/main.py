484d5187070810077d1a5aa071dafe26a4371447
Caio S. Duarte <caiosduarte@protonmail.com>
2025-12-05 21:24:11 +0000

    att

--- Aula 4 - For e Range/main.py
+++ Aula 4 - For e Range/main.py
@@ -42,3 +42,10 @@
 # for numero in range(1, 101):
 #     soma += numero
 # print('Soma de 1 a 100:', soma)
+
+#fatorial de input
+n = int(input('Digite um número para calcular o fatorial: '))
+fatorial = 1
+for numero in range(1, n + 1):
+    fatorial *= numero
+print(f'O fatorial de {n} é {fatorial}')
