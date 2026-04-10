d2799b420524b621062e597bc870a84311c6eab4
Caio S. Duarte <caiosduarte@protonmail.com>
2026-04-10 21:18:05 +0000

    att

--- Aula12 - POO II/main.py
+++ Aula12 - POO II/main.py
@@ -1,45 +1,74 @@
-class Disciplina:
+class Pessoa:
     def __init__(self, nome):
         self.__nome = nome
-        
+
     @property
     def nome(self):
         return self.__nome
+    
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.__nome}")
 
-class Aluno:
+class Professor(Pessoa):
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.nome} e eu ensino aqui.")
+
+class Aluno(Pessoa):
+    def __init__(self, nome, nota):
+        super().__init__(nome)
+        self.__nota = nota
+
+        if nota >= 7:
+            self.__status = 'Aprovado'
+        else:
+            self.__status = 'Reprovado'
+            
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.nome} e eu estudo aqui.")
+    
+    @property
+    def nota(self):
+        return self.__nota
+    
+    @nota.setter
+    def set_nota(self, nova_nota):
+        if nova_nota >= 0 and nova_nota <= 10:
+            self.__nota = nova_nota
+        else:
+            raise ValueError("A nota precisa estar entre 0 e 10!")
+
+class Disciplina:
     def __init__(self, nome):
         self.__nome = nome
-        
+
     @property
     def nome(self):
         return self.__nome
 
 class Turma:
-    def __init__(self, nome):
-        self.__nome = nome
+    def __init__(self, turma):
+        self.__turma = turma
         self.__alunos = []
         self.__disciplinas = []
-        
+
     @property
     def alunos(self):
         if self.__alunos:
             for i in self.__alunos:
-                print(i.nome)
-        else:            print("Não há alunos cadastrados.")
+               print(i.nome)
+        else:
+            print("Não há alunos nessa turma!")
 
     def adicionar_aluno(self, objeto_aluno):
         if isinstance(objeto_aluno, Aluno):
             self.__alunos.append(objeto_aluno)
         else:
-            raise ValueError("Erro: O objeto não é uma instância da classe Aluno.")
-            
-# Criando instâncias de Aluno
-aluno1 = Aluno("João")
-aluno2 = Aluno("Maria")
-aluno3 = Aluno("Pedro")
-aluno4 = Aluno("Ana")
+            raise ValueError("Precisa ser um objeto instanciado da classe Aluno!")
+        
+aluno_1 = Aluno("Aline", 10)
+pessoa_1 = Pessoa("Pedro")
+professor_1 = Professor("Maria")
 
-turma_a = Turma("Turma A")
-
-turma_a.adicionar_aluno(aluno1)
-turma_a.adicionar_aluno(aluno2)
+aluno_1.apresentar()
+pessoa_1.apresentar()
+professor_1.apresentar()
